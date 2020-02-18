1dafe8161d52cc2af698a31879da60743b47730d
Flucky <fluckymendes@gmail.com>
2020-02-18 14:03:57 +0300

    Added script.js and have done modal-windows and first version of Feature-section slider

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Появление модального окна modal-writeus
+document.getElementById('modal_writeus_btn').onclick = function() {
+    document.getElementById('modal_writeus').classList.remove('visually-hidden');
+}
+// Скрытие модального окна modal-writeus
+document.getElementById('modal_writeus_btn_close').onclick = function() {
+    document.getElementById('modal_writeus').classList.add('visually-hidden');
+}
+
+// Появление модального окна modal-map
+document.getElementById('modal_map_btn').onclick = function() {
+    document.getElementById('modal_map').classList.remove('visually-hidden');
+}
+// Скрытие модального окна modal-map
+document.getElementById('modal_map_btn_close').onclick = function() {
+    document.getElementById('modal_map').classList.add('visually-hidden');
+}
+
+// Преключение стилей кнопок секции "Сервисы"
+var buttons = document.querySelectorAll('.features__bottom__buttons__btn');
+
+for (var button of buttons) {
+   button.addEventListener('click', function () {
+     buttons.forEach(i => i.classList.remove('features__bottom__buttons__btn-active'));
+   
+     this.classList.toggle('features__bottom__buttons__btn-active');
+   });
+};
+
+// Слайдер секции "Сервисы"
+var featuresText = document.querySelector('.features__bottom__text');
+
+document.getElementById('features_delivery_btn').onclick = function() {
+    document.getElementById('features_delivery').classList.remove('visually-hidden');
+    document.getElementById('features_guarantee').classList.add('visually-hidden');
+    document.getElementById('features_credit').classList.add('visually-hidden');
+}
+
+document.getElementById('features_guarantee_btn').onclick = function() {
+    document.getElementById('features_delivery').classList.add('visually-hidden');
+    document.getElementById('features_guarantee').classList.remove('visually-hidden');
+    document.getElementById('features_credit').classList.add('visually-hidden');
+}
+
+document.getElementById('features_credit_btn').onclick = function() {
+    document.getElementById('features_delivery').classList.add('visually-hidden');
+    document.getElementById('features_guarantee').classList.add('visually-hidden');
+    document.getElementById('features_credit').classList.remove('visually-hidden');
+}
